4e35bff438d2c0479c189dc00825a635c8bfcecc
giuliogallerini <giulio.gallerini.work@hotmail.com>
2016-11-22 22:34:28 +0100

    fixed style bugs

--- client/helpers/favourites-helper.js
+++ client/helpers/favourites-helper.js
@@ -1,6 +1,6 @@
 export const isVisible = (currentPage) => {
   let visibility = '', viewport = $( window ).width()
 
-  currentPage.name === 'favourites' || viewport > 800 ? visibility = 'visible'  : visibility = 'hidden'
+  currentPage.name === 'favourites' || viewport > 900 ? visibility = 'visible'  : visibility = 'hidden'
   $('.favourites-container').css({'visibility':visibility})
 }
